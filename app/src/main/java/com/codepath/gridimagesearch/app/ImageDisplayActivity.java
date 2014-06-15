5bb42b3962b7ffef741c17c222b912b5a3db0bea
work computer <tinawen@loaner15-mba.corp.dropbox.com>
2014-06-14 22:24:40 -0700

    share

--- app/src/main/java/com/codepath/gridimagesearch/app/ImageDisplayActivity.java
+++ app/src/main/java/com/codepath/gridimagesearch/app/ImageDisplayActivity.java
@@ -1,14 +1,28 @@
 package com.codepath.gridimagesearch.app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ShareActionProvider;
 
+import com.loopj.android.image.SmartImageTask;
 import com.loopj.android.image.SmartImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class ImageDisplayActivity extends ActionBarActivity {
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +30,67 @@ public class ImageDisplayActivity extends ActionBarActivity {
         setContentView(R.layout.activity_image_display);
         ImageResult imageResult = (ImageResult) getIntent().getSerializableExtra("result");
         SmartImageView ivImage = (SmartImageView) findViewById(R.id.ivResult);
-        ivImage.setImageUrl(imageResult.getFullUrl());
+        ivImage.setImageUrl(imageResult.getFullUrl(), new SmartImageTask.OnCompleteListener() {
+            @Override
+            public void onComplete() {
+                // Setup share intent now that image has loaded
+                setupShareIntent();
+            }
+        });
     }
 
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void setupShareIntent() {
+        // Fetch Bitmap Uri locally
+        ImageView ivImage = (ImageView) findViewById(R.id.ivResult);
+        Uri bmpUri = getLocalBitmapUri(ivImage); // see previous remote images section
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        if (miShareAction != null) {
+            miShareAction.setShareIntent(shareIntent);
+        }
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.image_display, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.miShare);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) item.getActionProvider();
+        // Return true to display menu
         return true;
     }
 
